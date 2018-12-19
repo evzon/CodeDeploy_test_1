d31bde0abb1f1f7fb02046e01009abfb95249dfb
Bas van Zon <vanzon2000@yahoo.com>
2018-12-19 13:00:17 +0100

    Add project files.

---
+++ CodeDeploy_test_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDeploy_test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("codedeploy test 1");
+
+            Console.ReadKey();
+        }
+    }
+}
